05e4c3618c7d91c5b5f0fdb28c7f5506dde2744f
Sebastian Österlund <sirmcx@gmail.com>
2014-11-28 00:26:01 +0100

    Added dataset with coordinates.
    Outputs graph with vertices at coordinate location.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3.4
 import sys
 from graph_tool.all import *
+from numpy import *
 
 def euclidean_steiner_tree(g):
     tree = min_spanning_tree(g)
     g.set_edge_filter(tree)
     return g
 
+def get_positions(g):
+    x = g.vertex_properties["x"].get_array()
+    y = g.vertex_properties["y"].get_array()
+    coord = dstack((x,y))
+    pos = g.new_vertex_property("vector<double>")
+    for i,v in enumerate(g.vertices()):
+        pos[v] = coord[0][i]
+    return pos
+
 def start():
     filename = sys.argv[1]
     g = load_graph(filename)
     solution = euclidean_steiner_tree(g)
 
-    pos = solution.new_vertex_property("vector<double>")
-    # Set all positions to (0,0)
-    for v in solution.vertices():
-        pos[v] = (0,0)
-
+    pos = get_positions(solution)
     graph_draw(solution, pos=pos, output_size=(800, 800), output="out.png")
     print("Solved for: " + filename)
 
